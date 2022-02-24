1dc947c3f4f8c26bd381a89bb56a305c1439ad33
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-02-24 10:28:53 +0600

    dd

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ const searchBtn=()=> {
     fetch(url)
     .then(res=>res.json())
     .then(data => search(data))
-    
-
 }
 
 const search= (data)=>{
@@ -13,42 +11,37 @@ const search= (data)=>{
     const meals = data.meals;
     // console.log(data.meals);
     for (const meal of meals) {
-        console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML =`
-        <div class="card">
+        <div onclick="single(${meal.idMeal})" class="card">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
           <p class="card-text">${meal.strInstructions.slice(0,200)} </p>
         </div>
       </div>
-      `   
+      `   ;
       searchResult.appendChild(div)
         }
-    
-    // return searchInput;
 }
-// const searchResult = (data) =>{
-//     const searchResult = document.getElementById('search-result');
-//     for (const meal of data.meals) {
-//         // console.log(meal);
-//         const div = document.createElement('div');
-//         div.classList.add('col');
-//         div.innerHTML =`
-//         <div class="card">
-//         <img src="..." class="card-img-top" alt="..." />
-//         <div class="card-body">
-//           <h5 class="card-title">Card title</h5>
-//           <p class="card-text">
-//             This is a longer card with supporting text below as a natural
-//             lead-in to additional content. This content is a little bit
-//             longer.
-//           </p>
-//         </div>
-//       </div>
-//       `
-            
-//         }
-// }
+const single= (mealId)=>{
+    const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => loadData(data))
+}
+const loadData = (data) =>{
+    // console.log(data.meals[0]);
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML =`
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0,100)}
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+    `;
+}
